ac344659b56925836e8a45ae3ff89636aa22ec9e
Nabin <n@binghimire.com>
2017-05-25 07:06:16 +0545

    second commit

---
+++ nrb.org.np.py
@@ -0,0 +1,23 @@
+from lxml import html
+from lxml.etree import XPath as xpath
+import requests
+
+
+page = requests.get('http://www.nrb.org.np/fxmexchangerate.php')
+html_content = html.fromstring(page.content)
+
+currencies = html_content.xpath('//tr//td//font/text()')
+
+print("Currency Indian Rupees")
+print('-----')
+print("Unit", currencies[8].strip())
+print("Buyiing", currencies[9].strip())
+print("Selling", currencies[10].strip())
+print('-----')
+
+for i in range(18,len(currencies)-8,4):
+    print("Currency", currencies[i].strip())
+    print('-----')
+    print("Unit", currencies[i+1].strip())
+    print("Buyiing", currencies[i+2].strip())
+    print("Selling",cu
